ce925800a6983460ea19c42cdc846e83a9c2c586
Angelina <angelina.hcortes@gmail.com>
2023-01-04 23:49:44 -0600

    Add variables and questions

--- script.js
+++ script.js
@@ -1,42 +1,26 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-
 var start = document.getElementById("start-btn");
 start.addEventListener("click", startQuiz);
 var timerEl = document.getElementById("timer");
 var secondsLeft = 60;
 var score = 0;
 var question = document.getElementById("question");
-var answers = document.getElementsByClassName
 //Variable that contains an object with questions and answers for the quiz
 var questionsAndAnswers = [
   {
-    title: "Random question",
+    question: "It is an example of boolean",
+    answers: ["True", "22", "Boolean", "undefined"],
+    rightAnswer: "True"
+  },
+  {
+    question: "Random question2",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
     rightAnswer: "Answer1"
   },
   {
-    title: "Random question",
+    question: "Random question3",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
     rightAnswer: "Answer1"
-  },  {
-    title: "Random question",
-    answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
-    rightAnswer: "Answer1"
-  },  {
-    title: "Random question",
-    answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
-    rightAnswer: "Answer1"
-  },  
+  },
 ];
 
 //Set timer function
@@ -54,8 +38,18 @@ function setTime() {
 function startQuiz() {
   setTime();
   start.classList.add("hide");
+  currentQuestion();
+}
+
+function currentQuestion() {
+  question.innerText = questionsAndAnswers.question;
 }
 
 function selectAnswer() {
   
 }
+
+function nextAnswer() {
+
+}
+
